c149fdd4296f5dcd8ab35443e9a82df57c84196c
Henriqueheruster <henriquezxt@gmail.com>
2023-12-15 08:59:34 -0300

    Inserção de JavaScript

---
+++ script.js
@@ -0,0 +1,8 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.titulo',{duration: 2000})
+sr.reveal('.img-1',{duration: 2000})
+sr.reveal('.texto',{duration: 3000})
+sr.reveal('#card-1',{duration:8000})
+sr.reveal('#card-2',{duration:9000})
+sr.reveal('#card-3',{duration:10000})
