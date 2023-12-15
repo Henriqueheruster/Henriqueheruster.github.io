44f3862ba0a239e6eb6b7a2ff9b8273096aa7a6b
Henriqueheruster <henriquezxt@gmail.com>
2023-12-15 10:45:26 -0300

    concertando responsividade

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ sr.reveal('.texto',{duration: 3000})
 sr.reveal('#card-1',{duration:8000})
 sr.reveal('#card-2',{duration:9000})
 sr.reveal('#card-3',{duration:10000})
+
+function menuShow(){
+    let menuMobile = document.querySelector('.mobile-menu-icone')
+    if (menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+        document.querySelector('.icone').src = "./img/close_white_36dp.svg";
+    } else{
+        menuMobile.classList.add('open');
+        document.querySelector('.icone').src = "./img/Menu.svg";
+    }
+}
